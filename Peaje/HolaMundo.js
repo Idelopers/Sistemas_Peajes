c2ead9546bfe03f9818da08fcf18428bd14dfd98
Jose Alvarez <josefrank21@gmail.com>
2017-06-27 09:11:23 -0600

    Prueba de cambios

--- Peaje/HolaMundo.js
+++ Peaje/HolaMundo.js
@@ -1,9 +1,9 @@
 var http = require('http');
-var dts = require('./modulo1.js');
+var dt = require('./modulo1.js');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + dts.myDateTime());
+    res.write("The date and time are currently: " + dt.myDateTime());
     res.end();
 }).listen(80);
 
