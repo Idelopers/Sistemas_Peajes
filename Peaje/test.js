367bf9aff964a6485eb96de7ef60d782a1f1a289
Jose Alvarez <josefrank21@gmail.com>
2017-06-27 19:03:45 -0600

    Pruebas desde el Server

--- Peaje/test.js
+++ Peaje/test.js
@@ -3,6 +3,6 @@ var dt = require('./modulo1.js');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + dt.myDateTime());
+    res.write("Esta hora tiene su hora actual: " + dt.myDateTime());
     res.end();
 }).listen(80);
